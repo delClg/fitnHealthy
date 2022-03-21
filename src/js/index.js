5889f178283ad98b350bf7dab7bc9042d09e0d07
flyingdelta <flyingdelta@outlook.com>
2022-03-22 00:45:56 +0530

    Bug Removal1 && menu outside click

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,9 @@ menuBtn.addEventListener("click", () => {
   if (subMenu.style.display == "block") subMenu.style.display = "none";
   else subMenu.style.display = "block";
 });
+
+window.addEventListener("click", function (e) {
+  if (!subMenu.contains(e.target) && !menuBtn.contains(e.target)) {
+    subMenu.style.display = "none";
+  }
+});
