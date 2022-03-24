a621119d36cfae689803797e6057a92e3603e4a5
flyingdelta <flyingdelta@outlook.com>
2022-03-24 20:22:38 +0530

    Added Workouts & changed index.html & created templates

---
+++ src/js/template.js
@@ -0,0 +1,13 @@
+const menuBtn = document.getElementById("menu");
+const subMenu = document.getElementById("subMenu");
+
+menuBtn.addEventListener("click", () => {
+  if (subMenu.style.display == "block") subMenu.style.display = "none";
+  else subMenu.style.display = "block";
+});
+
+window.addEventListener("click", function (e) {
+  if (!subMenu.contains(e.target) && !menuBtn.contains(e.target)) {
+    subMenu.style.display = "none";
+  }
+});
